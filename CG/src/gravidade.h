af24b5f15892951256be6534e2e1b668383fd7d6
Victor Henrique <victorh1705@gmail.com>
2018-09-15 20:30:37 -0300

    https://github.com/victorh1705/Trabalho02_CG/projects/1#card-12946487
    Colisao

--- CG/src/gravidade.h
+++ CG/src/gravidade.h
@@ -5,6 +5,10 @@ float velocidade = 1.0;
 float gravidade = -10.0;
 float dt = 0.1;
 
+vector<Plano *> colisaoPreto;
+vector<Plano *> colisaoVermelho;
+
+vector<Plano *> planosPosicaoZ(float z, vector<Plano *> planos);
 void updateVelocidadePosicao();
 void idle();
 bool haColisao();
@@ -23,38 +27,55 @@ void idle()
     /// Calculate frame time
     frameTime = t - tLast;
     /// Calculate desired frame time
-    desiredFrameTime = 1.0 / (float) (desiredFPS);
+    desiredFrameTime = 1.0 / (float)(desiredFPS);
 
     // Check if the desired frame time was achieved. If not, skip animation.
-    if( frameTime <= desiredFrameTime)
+    if (frameTime <= desiredFrameTime)
         return;
 
     // **  UPDATE ANIMATION VARIABLES ** //
     float step = 1; // Speed of the animation
 
     updateVelocidadePosicao();
+    vector<Plano *> colisaoVermelho = planosPosicaoZ(-60.0, vermelho);
+    vector<Plano *> colisaoPreto = planosPosicaoZ(-60.0, preto);
 
     tLast = t;
 
     glutPostRedisplay();
 }
 
+void updateVelocidadePosicao()
+{
 
-void updateVelocidadePosicao(){
-
-    if(zBola <= 0.0){
+    if (zBola <= 0.0)
+    {
         velocidade = 25;
     }
 
-    velocidade = velocidade + gravidade*dt;
-    zBola = esfera->GetposicaoZ() + (velocidade*dt) + 0.5*gravidade*(dt*dt);
+    velocidade = velocidade + gravidade * dt;
+    zBola = esfera->GetposicaoZ() + (velocidade * dt) + 0.5 * gravidade * (dt * dt);
     esfera->SetposicaoZ(zBola);
 }
 
-bool haColisao(){
+bool haColisao()
+{
     return false;
 }
 
+vector<Plano *> planosPosicaoZ(float z, vector<Plano *> planos)
+{
+    vector<Plano *> retorno;
 
+    for (float i = 0; i <= planos.size(); i++)
+    {
+        Plano *it = vermelho[0];
+        if (it->GetposicaoZ() == z)
+        {
+            retorno.push_back(it);
+        }
+    }
+    return retorno;
+}
 
 #endif // GRAVIDADE_H_INCLUDED
